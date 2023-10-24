737709044cc51f36fae14498df17ad78342abb6f
CodeSandbox <support@codesandbox.io>
2023-10-24 10:55:07 +0000

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'auto',
+};
+
+export default config;
